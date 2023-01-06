2ecda4e7cc91df07892b93ccf89cc855c1e861c8
Adam <adamoctav@gmail.com>
2023-01-06 22:18:43 +0200

    handle user-data.json defaults

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { app } from "electron";
 const STORAGE_DATA_DEFAULT: Record<string, any> = {
   "main-display": null,
-  "widgets": [],
+  widgets: [],
 };
 
 const writeStorage = (data: Record<string, any>): void => {
@@ -13,14 +13,22 @@ const writeStorage = (data: Record<string, any>): void => {
   );
 };
 const retriveStorage = (): Record<string, any> => {
-  const userData = fs.readFileSync(
-    path.join(app.getAppPath(), "/user-data.json")
-  );
+  let userData;
   try {
-    const data = JSON.parse(userData.toString()); 
-    return {...STORAGE_DATA_DEFAULT,...data};
+    userData = fs.readFileSync(path.join(app.getAppPath(), "/user-data.json"));
   } catch (e) {
-    return {...STORAGE_DATA_DEFAULT};
+    fs.writeFileSync(
+      path.join(app.getAppPath(), "/user-data.json"),
+      JSON.stringify(STORAGE_DATA_DEFAULT)
+    );
+  }
+  try {
+    const data = userData
+      ? JSON.parse(userData.toString())
+      : STORAGE_DATA_DEFAULT;
+    return { ...STORAGE_DATA_DEFAULT, ...data };
+  } catch (e) {
+    return { ...STORAGE_DATA_DEFAULT };
   }
 };
 
